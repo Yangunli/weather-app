f1f928b136e0ba66caa9402aebff373b2fdd587a
Yangunli <yangun410@gmail.com>
2022-05-20 19:19:11 +0800

    Finish Weather App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+//import { ThemeProvider } from "emotion-theming"; 已失效 正確寫法from "@emotion/react";
+import { ThemeProvider } from "@emotion/react";
+import WeatherCard from "./views/WeatherCard";
+import WeatherSetting from "./views/WeatherSetting";
+import { getMoment, findLocation } from "./utils/helpers";
+import useWeatherAPI from "./hooks/useWeatherAPI";
+import styled from "@emotion/styled";
+
+const theme = {
+  light: {
+    backgroundColor: "#ededed",
+    foregroundColor: "#f9f9f9",
+    boxShadow: "0 1px 3px 0 #999999",
+    titleColor: "#212121",
+    temperatureColor: "#757575",
+    textColor: "#828282",
+  },
+  dark: {
+    backgroundColor: "#1F2022",
+    foregroundColor: "#121416",
+    boxShadow:
+      "0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)",
+    titleColor: "#f9f9fa",
+    temperatureColor: "#dddddd",
+    textColor: "#cccccc",
+  },
+};
+
+const Container = styled.div`
+  background-color: ${({ theme }) => theme.backgroundColor};
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AUTHORIZATION_KEY = "CWB-B3809DDF-DDA6-404C-A109-D1055501A93F";
+
+const App = () => {
+  const [currentTheme, setCurrentTheme] = useState("light");
+  //一開始進去畫面是沒有localstorage的紀錄，會跑不出東西！ 所以要給他default value
+  const storageCity = localStorage.getItem("cityName") || "臺北市";
+  const handleCurrentCityChange = (currentCity) => {
+    setCurrentCity(currentCity);
+  };
+
+  const [currentCity, setCurrentCity] = useState(storageCity);
+  const currentLocation = useMemo(
+    () => findLocation(currentCity),
+    [currentCity]
   );
-}
+  const { cityName, locationName, sunriseCityName } = currentLocation;
+  const moment = useMemo(() => getMoment(sunriseCityName), [sunriseCityName]);
+  useEffect(() => {
+    setCurrentTheme(moment === "day" ? "light" : "dark");
+  }, [moment]);
+  const [weatherElement, fetchData] = useWeatherAPI({
+    //useWeatherAPI used,APP used
+    locationName,
+    cityName,
+    authorizationKey: AUTHORIZATION_KEY,
+  });
+
+  const [currentPage, setCurrentPage] = useState("WeatherCard");
+  const handleCurrentPageChange = (currentPage) => {
+    setCurrentPage(currentPage);
+  };
+
+  return (
+    <ThemeProvider theme={theme[currentTheme]}>
+      <Container>
+        {currentPage === "WeatherCard" && (
+          <WeatherCard
+            cityName={cityName}
+            weatherElement={weatherElement}
+            moment={moment}
+            fetchData={fetchData}
+            handleCurrentPageChange={handleCurrentPageChange}
+          />
+        )}
+
+        {currentPage === "WeatherSetting" && (
+          <WeatherSetting
+            cityName={cityName}
+            handleCurrentPageChange={handleCurrentPageChange}
+            handleCurrentCityChange={handleCurrentCityChange}
+          />
+        )}
+      </Container>
+    </ThemeProvider>
+  );
+};
+/*
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);*/
 
 export default App;
---
+++ src/hooks/useWeatherAPI.js
@@ -0,0 +1,96 @@
+import { useEffect, useState, useCallback } from "react";
+
+const fetchCurrentWeather = ({ authorizationKey, locationName }) => {
+  return fetch(
+    `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=${authorizationKey}&locationName=${locationName}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // STEP 1：定義 `locationData` 把回傳的資料中會用到的部分取出來
+      const locationData = data.records.location[0];
+
+      // STEP 2：將風速（WDSD）和氣溫（TEMP）的資料取出
+      const weatherElements = locationData.weatherElement.reduce(
+        (neededElements, item) => {
+          if (["WDSD", "TEMP"].includes(item.elementName)) {
+            neededElements[item.elementName] = item.elementValue;
+          }
+          return neededElements;
+        },
+        {}
+      );
+
+      // STEP 3：要使用到 React 組件中的資料
+      return {
+        observationTime: locationData.time.obsTime,
+        locationName: locationData.locationName,
+        temperature: weatherElements.TEMP,
+        windSpeed: weatherElements.WDSD,
+        isLoading: false,
+      };
+    });
+};
+
+const fetchWeatherForecast = ({ authorizationKey, cityName }) => {
+  return fetch(
+    `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${authorizationKey}&locationName=${cityName}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const locationData = data.records.location[0];
+      const weatherElements = locationData.weatherElement.reduce(
+        (neededElements, item) => {
+          if (["Wx", "PoP", "CI"].includes(item.elementName)) {
+            neededElements[item.elementName] = item.time[0].parameter;
+          }
+          return neededElements;
+        },
+        {}
+      );
+      return {
+        //parameterName & parameterValue are from Response body
+
+        description: weatherElements.Wx.parameterName,
+        weatherCode: weatherElements.Wx.parameterValue,
+        rainPossibility: weatherElements.PoP.parameterName,
+        comfortability: weatherElements.CI.parameterName,
+      };
+    });
+};
+
+const useWeatherAPI = ({ locationName, cityName, authorizationKey }) => {
+  const [weatherElement, setWeatherElement] = useState({
+    observationTime: new Date(),
+    locationName: "",
+    temperature: 0,
+    windSpeed: 0,
+    description: "",
+    weatherCode: 0,
+    rainPossibility: 0,
+    comfortability: "",
+    isLoading: true,
+  });
+
+  const fetchData = useCallback(async () => {
+    setWeatherElement((prevState) => ({
+      ...prevState,
+      isLoading: true,
+    }));
+    const [currentWeather, weatherForecast] = await Promise.all([
+      fetchCurrentWeather({ authorizationKey, locationName }),
+      fetchWeatherForecast({ authorizationKey, cityName }),
+    ]);
+    setWeatherElement({
+      ...currentWeather,
+      ...weatherForecast,
+      isLoading: false,
+    });
+  }, [authorizationKey, locationName, cityName]);
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return [weatherElement, fetchData];
+};
+
+export default useWeatherAPI;
